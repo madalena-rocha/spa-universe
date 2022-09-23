3af0a80fe69dc24420d468b291437c1f9d1475d1
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-09-23 15:02:02 -0300

    add error 404 files

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const router = new Router()
 router.add("/", "/pages/home.html")
 router.add("/universe", "/pages/universe.html")
 router.add("/exploration", "/pages/exploration.html")
+router.add(404, "/pages/404.html")
 
 router.handle()
 

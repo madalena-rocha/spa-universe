3798ca15c68396c2d5db479a165a81eafa68fa88
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-09-23 10:02:45 -0300

    add js files

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import { Router } from "./router.js"
+
+const router = new Router()
+router.add("/", "/pages/home.html")
+router.add("/universe", "/pages/universe.html")
+router.add("/exploration", "/pages/exploration.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
